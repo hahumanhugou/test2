966b6fb7da6bf6411719bea53a338308c88409c1
hahumanhugou <132132136+hahumanhugou@users.noreply.github.com>
2023-05-11 11:24:05 +0000

    [Change]test2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
   def index
-
+     @book = Book.new
+     @books = Book.all
   end
   def edit
 
   end
   def show
-
+     @book = Book.find(params[:id])
+  end
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+  end
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
