c2c1ba8ca65386d75592643ce3668279e2c1726c
hahumanhugou <132132136+hahumanhugou@users.noreply.github.com>
2023-05-16 14:22:34 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,40 @@ class BooksController < ApplicationController
      @book = Book.new
      @books = Book.all
   end
-  def edit
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  def update
+    @book = Book.find(params[:id])
+    if  @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path(@book.id)
+    else
 
+       render :edit
+    end
+  end
+  def edit
+     @book = Book.find(params[:id])
   end
   def show
      @book = Book.find(params[:id])
   end
-  def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-   private
+  private
   # ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
